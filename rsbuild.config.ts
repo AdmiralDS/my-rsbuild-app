d4c9cdf21cf9aff2fed66ab09670119ce32af7a3
Andrey Litvinov <i@drune.ru>
2025-02-20 15:40:36 +0300

    add styled components and svgr

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from '@rsbuild/core';
+
+// https://rsbuild.dev/plugins/list/plugin-react
 import { pluginReact } from '@rsbuild/plugin-react';
 
+// https://rsbuild.dev/plugins/list/plugin-svgr
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
+
+// https://github.com/rspack-contrib/rsbuild-plugin-styled-components
+import { pluginStyledComponents } from "@rsbuild/plugin-styled-components";
+
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(), pluginSvgr(), pluginStyledComponents({displayName:true})],
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { LIGHT_THEME, FontsVTBGroup, DropdownProvider } from '@admiral-ds/react-ui';
 import App from './App';
+import './index.css';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+    <ThemeProvider theme={LIGHT_THEME}>
+      <DropdownProvider>
+        <FontsVTBGroup />
+        <App />
+      </DropdownProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
   );
 }
